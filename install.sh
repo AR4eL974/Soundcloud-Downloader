e54fb860c8242e031afad1c5400c45be676b0cf9
AR4eL <massonalexandre8@gmail.com>
2026-03-25 14:23:21 +0400

    typos

--- install.sh
+++ install.sh
@@ -18,22 +18,28 @@ arch_installation(){
         echo ""
         echo ""
         echo "Dependencies properly installed"
+        sleep 1
         echo ""
         echo ""
         echo "getting the last version of SoundCloud Downloader from github..."
+        sleep 1
         git clone https://github.com/AR4eL974/Soundcloud-Downloader.git
         echo ""
         echo "repo successfully cloned"
+        sleep 1
         echo ""
         echo "extracting the executable from the repo directory and deleting the directory"
         cp Soundcloud-Downloader/soundcloud.sh soundcloud.sh
         rm -rf Soundcloud-Downloader
+        sleep 1
         echo ""
         echo ""
         echo "giving execution permissions to the executable"
         chmod +x soundcloud.sh
+        sleep 1
         echo ""
         echo "installation complete, you may want to delete this installer by running 'rm install.sh' or deleting it via your file explorer"
+        sleep 1
 }
 os_detect
 
